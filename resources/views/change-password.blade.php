65756c1892e05d391cd788f7b8babdada5eaf24e
azuma95 <8ctiagopereira@gmail.com>
2022-04-23 21:06:56 +0100

    add change password functionality

---
+++ resources/views/change-password.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Change Password') }}</div>
+
+                    <form action="{{ route('update-password') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @elseif (session('error'))
+                                <div class="alert alert-danger" role="alert">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+                            
+                        <div class="form-group">
+                            <label for="oldPasswordInput" class="form-label">Old Password</label>
+                            <input name="old_password" type="password" class="form-control" id="oldPasswordInput" placeholder="Old Password">
+
+                            @error('old_password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+
+                        </div>
+                        <div class="form-group">
+                            <label for="newPasswordInput" class="form-label">New Password</label>
+                            <input name="new_password" type="password" class="form-control" id="newPasswordInput" placeholder="New Password">
+
+                            @error('new_password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmNewPasswordInput" class="form-label">Confirm New Password</label>
+                            <input name="new_password_confirmation" type="password" class="form-control" id="confirmnewPasswordInput" placeholder="Confirm New Password">
+                        </div>
+                        <button type="submit" style="margin-top: 10px;" class="btn btn-primary">Submit</button>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
